5d9329d8b09358797e6b9d89667e9365eb52bd40
Levi Bostian <levi.bostian@gmail.com>
2014-10-04 12:44:00 -0500

    Remove navigation drawer.

---
+++ me2/app/src/main/java/co/me2app/me2/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package co.me2app.me2.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import co.me2app.me2.R;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ me2/app/src/main/java/co/me2app/me2/fragment/MainFragment.java
@@ -0,0 +1,23 @@
+package co.me2app.me2.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import co.me2app.me2.R;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance(int sectionNumber) {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
