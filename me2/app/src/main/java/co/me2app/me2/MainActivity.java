725c3e4b26b24bf4ea7a5481de24bfbf885ecc6e
Levi Bostian <levi.bostian@gmail.com>
2014-10-04 12:28:56 -0500

    Add Parse.

--- me2/app/src/main/java/co/me2app/me2/MainActivity.java
+++ me2/app/src/main/java/co/me2app/me2/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import com.parse.ParseObject;
 
 
 public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
