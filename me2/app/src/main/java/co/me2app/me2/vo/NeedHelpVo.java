17bdb6291bccc64626ad90fe1f9f32d9826a3bb5
Levi Bostian <levi.bostian@gmail.com>
2014-10-05 11:04:00 -0500

    Insert identification icon into rows.

--- me2/app/src/main/java/co/me2app/me2/vo/NeedHelpVo.java
+++ me2/app/src/main/java/co/me2app/me2/vo/NeedHelpVo.java
@@ -2,13 +2,14 @@ package co.me2app.me2.vo;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import co.me2app.me2.R;
 
 public class NeedHelpVo implements Parcelable {
     public int statusColor = android.R.color.holo_blue_bright;
     public String username = "super cheese";
     public String date = "5 hours ago";
     public String struggle = "Remain lively hardly needed at do by. Two you fat downs fanny three. True mr gone most at. Dare as name just when with it body. Remain lively hardly needed at do by. Two you fat downs fanny three. True mr gone most at. Dare as name just when with it body. Remain lively hardly needed at do by. Two you fat downs fanny three. True mr gone most at. Dare as name just when with it body. Travelling inquietude she increasing off impossible the. Cottage be noisier looking to we promise on. Disposal to kindness appetite diverted learning of on raptures. Betrayed any may returned now dashwood formerly. Balls way delay shy boy man views. No so instrument discretion unsatiable to in. Repulsive questions contented him few extensive supported. Of remarkably thoroughly he appearance in. Supposing tolerably applauded or of be. Suffering unfeeling so objection agreeable allowance me of. Ask within entire season sex common far who family. As be valley warmth assure on. Park girl they rich hour new well way you. Face ye be me been room we sons fond. ";
-    public int userGender = android.R.color.holo_orange_dark;
+    public int userGender = R.drawable.user_male;
     public String location = "Iowa, USA";
 
     @Override
