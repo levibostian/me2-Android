24b580afe3dc90925f31f33db8af57bd496b5943
Larry Boateng Asante <boatenga17@grinnell.edu>
2014-10-05 16:29:26 -0500

    UI Overhaul

--- me2/app/src/main/java/co/me2app/me2/activity/LogInActivity.java
+++ me2/app/src/main/java/co/me2app/me2/activity/LogInActivity.java
@@ -1,12 +1,12 @@
 package co.me2app.me2.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
@@ -28,13 +28,13 @@ public class LogInActivity extends FragmentActivity {
     EditText passwordField;
     EditText userEmailField;
     Button signUpButton, logInButton;
-    ProgressBar loading;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
+        getActionBar().hide();
 
         // Reading secret ids for Parse from file
         String applicationId = "";
@@ -51,70 +51,88 @@ public class LogInActivity extends FragmentActivity {
         // Initialize Parse
         Parse.initialize(this, applicationId, clientKey);
 
+        if (ParseUser.getCurrentUser() != null) {
+            Intent toMainApp = new Intent(LogInActivity.this, MainActivity.class);
+            startActivity(toMainApp);
+            finish();
+
+        } else {
+            super.onResume();
+        }
+
         // Reference Views
         userEmailField = (EditText) findViewById(R.id.emailField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         signUpButton = (Button) findViewById(R.id.signUpButton);
         logInButton = (Button) findViewById(R.id.logInButton);
-        loading = (ProgressBar) findViewById(R.id.progressBar);
-        loading.setVisibility(View.INVISIBLE);
+
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
+            // Email Validator
+            public final boolean isValidEmail(CharSequence target) {
+                if (target == null) {
+                    return false;
+                } else {
+                    return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+                }
+            }
+
             @Override
             public void onClick(View v) {
-                logInButton.setVisibility(View.INVISIBLE);
-                signUpButton.setVisibility(View.INVISIBLE);
-
-                if (email.equals("") || password.equals("")) {
-                    logInButton.setVisibility(View.VISIBLE);
-                    signUpButton.setVisibility(View.VISIBLE);
-
-                    Toast.makeText(LogInActivity.this, "Please complete credentials", Toast.LENGTH_LONG).show();
-
-                    return;
-                }
-
+                // eliminate whitespace and store details
                 password = passwordField.getText().toString().trim();
                 email = userEmailField.getText().toString().trim();
                 Log.d("Tag", email);
 
 
-                final ParseUser newUser = new ParseUser();
+                // Check for empty fields
+                if (email.equals("") || password.equals("")) {
+                    Toast.makeText(LogInActivity.this, "Please fill in all credentials", Toast.LENGTH_LONG).show();
+                    return;
+                } // ends if
+
+
+                // Use user details to create a new Parse User
+                ParseUser newUser = new ParseUser();
                 newUser.setUsername(email);
                 newUser.setPassword(password);
 
+
+                // Test for input values
+                Log.d("Username", newUser.getUsername());
+
+                // Sign Up user in the background
                 newUser.signUpInBackground(new SignUpCallback() {
 
                     @Override
                     public void done(ParseException e) {
-                        //loading.setVisibility(View.INVISIBLE);
-                        ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
-                        bar.setVisibility(View.VISIBLE);
-                        if (e == null) {
-                            bar.setVisibility(View.INVISIBLE);
-                            Toast.makeText(LogInActivity.this, "To Main", Toast.LENGTH_LONG).show();
 
-//                           Intent toMainActivity = new Intent(LogInActivity.this, MainActivity.class);
-//                           startActivity(toMainActivity);
-//                           finish();
+
+                        if (isValidEmail(email) == true) {
+                            if (e == null) {
+                                Toast.makeText(LogInActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+                                Intent toMainActivity = new Intent(LogInActivity.this, MainActivity.class);
+                                startActivity(toMainActivity);
+                                finish();
+                            } else {
+                                Log.d("error", e.getMessage());
+                                Toast.makeText(LogInActivity.this, e.getMessage() + "." + " " + "Please use a different email as your username", Toast.LENGTH_LONG).show();
+                            }
                         } else {
-                            bar.setVisibility(View.INVISIBLE);
-                            Log.d("Tag2", newUser.getUsername());
-                            Toast.makeText(LogInActivity.this, "whoops", Toast.LENGTH_LONG).show();
-                        }
-                    } // done(e)
-                }); //newUser.signUpInBackground(new SignUpCallback(){})
+                            Toast.makeText(LogInActivity.this, "Invalid Email Address", Toast.LENGTH_LONG).show();
+                        } // done(e)
+
+                    }
+                });
+
             } // onClick(v)
         }); // setOnClickListener(new View.OnClickListener())
 
+
         // Sets action for user on clicking sign in button
         logInButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                logInButton.setVisibility(View.INVISIBLE);
-                signUpButton.setVisibility(View.INVISIBLE);
-                // loading.setVisibility(View.INVISIBLE);
 
                 // Get text from views and eliminate whitespace
                 password = passwordField.getText().toString().trim();
@@ -127,8 +145,6 @@ public class LogInActivity extends FragmentActivity {
 
                 // Check if user creds are empty. Report friendly message
                 if (email.equals("") || password.equals("")) {
-                    logInButton.setVisibility(View.VISIBLE);
-                    signUpButton.setVisibility(View.VISIBLE);
                     Toast.makeText(LogInActivity.this, "Please complete credentials", Toast.LENGTH_LONG).show();
                     return;
                 }
@@ -136,36 +152,24 @@ public class LogInActivity extends FragmentActivity {
                 // Sign User in
                 ParseUser.logInInBackground(email, password, new LogInCallback() {
                     public void done(ParseUser user, ParseException e) {
-
                         // Check if user exists
                         if (user != null) {
-                            //   loading.setVisibility(View.INVISIBLE);
-//                            Intent toMainActivity = new Intent(LogInActivity.this, MainActivity.class);
-//                            startActivity(toMainActivity);
-//                            finish();
+
                             Toast.makeText(LogInActivity.this, "Signed In!", Toast.LENGTH_LONG).show();
+                            Intent toMainActivity = new Intent(LogInActivity.this, MainActivity.class);
+                            startActivity(toMainActivity);
+                            finish();
+
                         }// closes if
                         else {
-                            logInButton.setVisibility(View.VISIBLE);
-                            signUpButton.setVisibility(View.VISIBLE);
-                            Toast.makeText(LogInActivity.this, "Please check your credentials", Toast.LENGTH_LONG).show();
+                            Toast.makeText(LogInActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                         } // closes else
 
                         //TODO: Check error messages if e generates any errors and fix tests
 
-//                        if (e.getMessage().equals(ParseException.EMAIL_NOT_FOUND)){
-//                            Toast.makeText(LogInActivity.this, "E-mail not found. Please use an existing email", Toast.LENGTH_LONG).show();
-//                        }
-//                        if (e.getMessage().equals(ParseException.INVALID_EMAIL_ADDRESS)){
-//                            Toast.makeText(LogInActivity.this, "Invalid Email. Please check and rectify error", Toast.LENGTH_LONG).show();
-//                        }
-//                        if (e.getMessage().equals(ParseException.EMAIL_MISSING)){
-//                            Toast.makeText(LogInActivity.this, "Please complete your credentials", Toast.LENGTH_LONG).show();
-//                        }
                     } // done(user, e)
                 }); // user.logInInBackground(email, password, new LogInCallBack(){})
             }
         });
     } // onCreate(savedBundleState)
-
 } // LogInActivity
